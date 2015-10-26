b2b5f4db7208efa21010ced49587ae60f2063746
wareya <wareya@gmail.com>
2015-10-26 02:35:21 -0400

    Make changes that work around FnFg "removeOverlap" bugs

--- makefont.py
+++ makefont.py
@@ -13,8 +13,12 @@ print("Generating...")
 for name in list(filter(lambda name: re.match("0....\.svg", name) , os.listdir())):
     glyph = font.createMappedChar("u"+name[1:5])
     glyph.importOutlines(name)
+    glyph.round()
     glyph.removeOverlap()
-    glyph.correctDirection()
+    # The only purpose of rounding up above is to avoid bugs in "removeOverlap".
     glyph.simplify(2,("smoothcurves", "mergelines", "setstarttoextremum","removesingletonpoints"))
+    # Simplifying might take care of any extra, minor errors (such as overlapping points).
+    glyph.removeOverlap()
+    # We removeoverlap again just to be safe.
 print("Saving...")
 font.generate('testfont.otf')
